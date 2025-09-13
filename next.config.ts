91caa3c2e7462475ae4e6641ae582fcc59790baf
Samarth D Kolur <145587928+samarthkolur@users.noreply.github.com>
2025-09-13 20:18:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  output: 'export',
+}
 };
 
 export default nextConfig;
